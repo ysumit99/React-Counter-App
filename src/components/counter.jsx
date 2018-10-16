e4f47c95439ad4e7f867fdcfcde4a2fd61d5d391
ysumit99 <ysumit99@gmail.com>
2018-10-16 12:27:03 +0530

    Passing data to components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
     tags: ["tag1", "tag2", "tag3"]
   };
 
@@ -16,8 +16,8 @@ class Counter extends Component {
 
   // This is an alternative to above constructor , used for binding events
   handleIncrement = product => {
-    console.log(product);
-    this.setState({ count: this.state.count + 1 });
+    //console.log(product);
+    this.setState({ value: this.state.value + 1 });
   };
 
   //   doHandleIncrement = () => {
@@ -30,8 +30,10 @@ class Counter extends Component {
   //   };
 
   render() {
+    console.log(this.props);
     return (
       <div>
+        {/* {this.props.children} */}
         <span className={this.getBadgeClasses()}> {this.formatCount()} </span>
         <button
           onClick={() => this.handleIncrement(1)}
@@ -45,13 +47,13 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
